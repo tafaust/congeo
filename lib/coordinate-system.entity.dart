6b7cff9de693514ad89af1b0398b29de4c5fe2bb
Thomas Hesse <thomas.hesse@neoloan.de>
2021-12-17 11:16:57 +0100

    feat: add more coordinate systems

--- lib/coordinate-system.entity.dart
+++ lib/coordinate-system.entity.dart
@@ -2,20 +2,52 @@ import 'package:proj4dart/proj4dart.dart';
 
 List<CoordinateSystem> allCoords = [
   CoordinateSystem(
-      epsgCode: 'EPSG:4326',
-      name: 'World Geodetic System 84',
-      description: 'Used for GPS and stuff.',
-      proj4: null),
+    epsgCode: 'EPSG:4088',
+    name: 'World Equidistant Cylindrical',
+    description:
+        'Uses spherical development. It is not a recognised geodetic system. Relative to an ellipsoidal development (see WGS 84 / World Equidistant Cylindrical (CRS code 4087)) errors of up to 800 metres in position and 0.7% in scale may arise. ',
+    unit: 'meter',
+  ),
   CoordinateSystem(
-      epsgCode: 'EPSG:3857',
-      name: 'Web Mercator Projection',
-      description: 'TODO',
-      proj4: null),
+    epsgCode: 'EPSG:4087',
+    name: 'WGS 84 / World Equidistant Cylindrical',
+    description:
+        'Origin is at intersection of equator and Greenwich meridian. Note: this is not the same as plotting unrectified graticule coordinates on a computer display using the so-called pseudo Plate Carrée method: here the grid units are metres. ',
+    unit: 'meter',
+  ),
   CoordinateSystem(
-      epsgCode: 'EPSG:7789',
-      name: 'International Terrestrial Reference Frame 2014',
-      description: 'TODO',
-      proj4: null),
+    epsgCode: 'EPSG:4326',
+    name: 'WGS 84',
+    description:
+        'Horizontal component of 3D system. Used by the GPS satellite navigation system and for NATO military geodetic surveying. ',
+    unit: 'degree',
+  ),
+  CoordinateSystem(
+    epsgCode: 'EPSG:3395',
+    name: 'WGS 84 / World Mercator',
+    description: 'Euro-centric view of world excluding polar areas.',
+    unit: 'meter',
+  ),
+  CoordinateSystem(
+    epsgCode: 'EPSG:6893',
+    name: 'WGS 84 / World Mercator + EGM2008 height',
+    description:
+        'Visualisation of initial concept in civil engineering design.',
+    unit: 'meter',
+  ),
+  CoordinateSystem(
+    epsgCode: 'EPSG:3857',
+    name: 'WGS 84 / Pseudo-Mercator',
+    description:
+        'Uses spherical development of ellipsoidal coordinates. Relative to WGS 84 / World Mercator (CRS code 3395) errors of 0.7 percent in scale and differences in northing of up to 43km in the map (equivalent to 21km on the ground) may arise. ',
+    unit: 'meter',
+  ),
+  CoordinateSystem(
+    epsgCode: 'EPSG:7789',
+    name: 'International Terrestrial Reference Frame 2014',
+    description: 'TODO',
+    unit: 'meter',
+  ),
 ]
     .where((CoordinateSystem coord) => Projection.get(coord.epsgCode) != null)
     .toList();
@@ -24,14 +56,16 @@ class CoordinateSystem {
   final String epsgCode;
   final String name;
   final String description;
-  final String? proj4;
+  final String unit;
+  // final String? proj4;
   late final Projection? projection;
 
   CoordinateSystem({
     required this.epsgCode,
     required this.name,
     required this.description,
-    required this.proj4,
+    required this.unit,
+    // required this.proj4,
   }) {
     projection = Projection.get(epsgCode);
   }
